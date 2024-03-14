dfcdea2a652bc5da150a5739cabde33715db61a0
SeamusSJU18 <146009842+SeamusSJU18@users.noreply.github.com>
2024-03-14 11:44:57 -0400

    Create view_script.js

---
+++ view_script.js
@@ -0,0 +1,17 @@
+var model = [
+    {
+        name: "Dr. Christoforou",
+        expertise: "Machine Learning, Artificial Intelligence; Brain-computer interfaces; neurall-informed predirective models",
+        profile: "https://scholar.google.com/citations?user=0PkTM-MAAAAJ&hl=en"
+    },
+    {
+        name: "Dr. Yadav",
+        expertise: "Mobile Computing, Computer Systems",
+        profile: "https://scholar.google.co.in/citations?user=0sCNwzEAAAAJ@ht=en"
+    },
+    {
+        name: "Dr. Keshtkar",
+        expertise: "Natural Language Processing",
+        profile: "https://scholar.google.com/citations?user=_yw77_sAAAAJ&ht=en"
+    }
+];
